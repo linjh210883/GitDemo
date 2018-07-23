14f256dd5314fec1c570420317b1f06ecefc555d
林剑航 <ljh827429783@icloud.com>
2018-07-23 23:52:13 +0800

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by 林剑航 on 2018/7/23.
+//  Copyright © 2018年 jiyuanSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by 林剑航 on 2018/7/23.
+//  Copyright © 2018年 jiyuanSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
